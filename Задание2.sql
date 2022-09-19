6d12836de5d3c90e5fad6d8248c42376d70068be
ALEXANDER CHUVAKOV <64259529+chuvakov@users.noreply.github.com>
2022-09-19 17:59:26 +0300

    Update Задание2.sql

--- Задание2.sql
+++ Задание2.sql
@@ -1,4 +1,53 @@
-select p.name, c.name
-from products as p 
-left join productscategories as pc on p.id = pc.productid 
-left join categories as c on pc.categoryid = c.id;
+CREATE DATABASE Shop;
+USE Shop;
+
+CREATE TABLE Categories
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(256)
+);
+
+CREATE TABLE Products
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(256)
+);
+
+CREATE TABLE CategoryProducts
+(
+	ProductId INT REFERENCES Products,
+	CategoryId INT REFERENCES Categories
+); 
+
+INSERT INTO Categories(Name) VALUES
+('category1'), 
+('category2'),
+('category3'),
+('category4'),
+('category5'),
+('category6');
+
+INSERT INTO Products(Name) VALUES
+('productCode1'),
+('productCode2'),
+('productCode3'),
+('productCode4'),
+('productCode5'),
+('productCode6'),
+('productCode7'),
+('productCode8'),
+('productCode9');
+
+INSERT INTO CategoryProducts VALUES
+(1,1),
+(2,1),
+(4,2),
+(5,2),
+(7,3),
+(8,4),
+(9,5);
+
+SELECT p.Name, c.Name 
+FROM Products p 
+LEFT JOIN CategoryProducts cp ON cp.ProductId = p.Id 
+LEFT JOIN Categories c ON c.Id = cp.CategoryId;
